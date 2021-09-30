b74d363cee39aad523aa78a32c45826f123ecd3f
Nathan <nathan.clough@ttu.edu>
2021-09-30 18:59:19 -0500

    Added final output for alternating bit

--- AlternatingBit.c
+++ AlternatingBit.c
@@ -21,7 +21,7 @@
 /* and write a routine called B_output */
 #define ACK 16
 #define NACK -16
-#define TIMEOUT 50.0
+#define TIMEOUT 100.0
 /* a "msg" is the data unit passed from layer 5 (teachers code) to layer  */
 /* 4 (students' code).  It contains the data (characters) to be delivered */
 /* to layer 5 via the students transport level protocol entities.         */
